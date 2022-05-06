5f7fb2e3ecac1f3fb60749755fd130ebe292f5af
Joffre Hermosilla Salas <joffre.hermosilla@gmail.com>
2022-05-06 05:25:48 -0500

    update kafka

---
+++ src/main/java/nttdata/bootcamp/microservicios/movimientos/cuentas/config/KafkaTopicConfig.java
@@ -0,0 +1,14 @@
+package nttdata.bootcamp.microservicios.movimientos.cuentas.config;
+
+import org.apache.kafka.clients.admin.NewTopic;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.kafka.config.TopicBuilder;
+
+//@Configuration
+public class KafkaTopicConfig {
+	/*
+	 * @Bean public NewTopic movementsAccountsTopic() { return
+	 * TopicBuilder.name("movementsaccounts").build(); }
+	 */
+}
